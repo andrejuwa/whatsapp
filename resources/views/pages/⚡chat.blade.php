3d874d4e7ffe6dd9a41e88aa655e682141f56bb8
andre wagner <andrejuwa2@gmail.com>
2026-03-24 14:00:49 -0300

    git init

---
+++ resources/views/pages/⚡chat.blade.php
@@ -0,0 +1,102 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component
+{
+    public $contatos = [];
+
+    public function mount()
+    {
+        $this->carregarMensagens();
+    }
+
+    public function carregarMensagens()
+    {
+        $this->contatos = [
+            "555596869456" => [
+                "name" => "Andre junior wagner - pessoal",
+                "mensagens" => [
+                    "24-03-2026 13:49:50" => [
+                        "remetente" => "eu",
+                        "destinatario" => "555596869456",
+                        "mensagem" => "Olá, como voce está?",
+                    ],
+                    "24-03-2026 13:50:55" => [
+                        "remetente" => "555596869456",
+                        "destinatario" => "eu",
+                        "mensagem" => "Estou bem e voce?",
+                    ]
+                ]
+            ]
+        ];
+    }
+};
+?>
+
+<div>
+
+
+    <!-- Static sidebar for desktop -->
+    <div class="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col dark:bg-gray-900">
+        <!-- Sidebar component, swap this element with another sidebar if you like -->
+        <div class="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 dark:border-white/10 dark:bg-black/10">
+            <div class="flex h-16 shrink-0 items-center">
+                <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" class="h-8 w-auto dark:hidden" />
+                <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" class="h-8 w-auto not-dark:hidden" />
+            </div>
+            <nav class="flex flex-1 flex-col">
+                <ul role="list" class="flex flex-1 flex-col gap-y-7">
+                    <li>
+                        <ul role="list" class="flex flex-1 flex-col gap-y-7">
+                            <li>
+                                <ul role="list" class="-mx-2 space-y-1">
+                                    @foreach($this->contatos as $contato)
+                                        <li>
+                                            <!-- Current: "bg-gray-50 dark:bg-white/5 text-indigo-600 dark:text-white", Default: "text-gray-700 dark:text-gray-400 hover:text-indigo-600 dark:hover:text-white hover:bg-gray-50 dark:hover:bg-white/5" -->
+                                            <a href="#" class="group flex gap-x-3 rounded-md bg-gray-50 p-2 text-sm/6 font-semibold text-indigo-600 dark:bg-white/5 dark:text-white">
+
+                                                {{ $contato['name'] }}
+                                            </a>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </li>
+
+                        </ul>
+                    </li>
+                    <li class="-mx-6 mt-auto">
+                        <a href="#" class="flex items-center gap-x-4 px-6 py-3 text-sm/6 font-semibold text-gray-900 hover:bg-gray-50 dark:text-white dark:hover:bg-white/5">
+                            <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="size-8 rounded-full bg-gray-50 outline -outline-offset-1 outline-black/5 dark:bg-gray-800 dark:outline-white/10" />
+                            <span class="sr-only">Your profile</span>
+                            <span aria-hidden="true">André Wagner</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+
+    <div class="sticky top-0 z-40 flex items-center gap-x-6 bg-white px-4 py-4 shadow-xs sm:px-6 lg:hidden dark:bg-gray-900 dark:shadow-none dark:after:pointer-events-none dark:after:absolute dark:after:inset-0 dark:after:border-b dark:after:border-white/10 dark:after:bg-black/10">
+        <button type="button" command="show-modal" commandfor="sidebar" class="-m-2.5 p-2.5 text-gray-700 hover:text-gray-900 lg:hidden dark:text-gray-400 dark:hover:text-white">
+            <span class="sr-only">Open sidebar</span>
+            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6">
+                <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+        </button>
+        <div class="flex-1 text-sm/6 font-semibold text-gray-900 dark:text-white">Dashboard</div>
+        <a href="#">
+            <span class="sr-only">Your profile</span>
+            <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="size-8 rounded-full bg-gray-50 outline -outline-offset-1 outline-black/5 dark:bg-gray-800 dark:outline-white/10" />
+        </a>
+    </div>
+
+    <main class="py-10 lg:pl-72">
+        <div class="px-4 sm:px-6 lg:px-8">
+            abc
+        </div>
+    </main>
+
+
+
+</div>
