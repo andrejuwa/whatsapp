1cdc58bb72ad65ca10ed3ba9d78d9c98f5be9625
andre wagner <andrejuwa2@gmail.com>
2026-03-30 10:55:45 -0300

    att

--- resources/views/pages/⚡arquivar.blade.php
+++ resources/views/pages/⚡arquivar.blade.php
@@ -8,7 +8,7 @@ new class extends Component
     public function mount($whatsappId)
     {
         \App\Models\Mensagem::query()->where('whatsapp_id', $whatsappId)->update(['mensagem_arquivada' => true]);
-        return $this->redirect('/'); // ou route('nome.da.rota')
+        $this->redirect(route('home'), navigate: true);
     }
 
 
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::livewire('/', 'pages::chat');
+Route::livewire('/', 'pages::chat')->name('home');
 Route::livewire('/contato/{waId}', 'pages::chat');
 Route::livewire('/arquivar/{whatsappId}', 'pages::arquivar');
